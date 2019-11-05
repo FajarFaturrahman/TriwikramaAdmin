967187b7258224a6d51a91ae9fb3954b502750cb
Fajar Faturrahman <fajarfaturrahman06@gmail.com>
2019-11-05 11:41:10 +0700

    commit untuk migration

---
+++ database/migrations/2019_11_05_032524_create_portofolio_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePortofolioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portofolio', function (Blueprint $table) {
+            $table->increments('id_portofolio');
+            $table->string('nama_aplikasi', 100);
+            $table->enum('tipe_website', ['Corporate', 'E-Commerce', 'Web App', 'Mobile App']);
+            $table->enum('platform',  ['Web Application', 'Mobile Aplication', 'Responsive Web Application']);
+            $table->string('domain_portofolio', 100);
+            $table->enum('status', ['active','expired']);
+            $table->longText('description');        
+            $table->integer('id_client')->unsigned();
+            $table->foreign('id_client')->references('id_client')->on('client');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('portofolio');
+    }
+}
