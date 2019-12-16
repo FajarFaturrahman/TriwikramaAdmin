a863a95d0031db599aa5778dc0330b2649892be5
Fajar Faturrahman <fajarfaturrahman06@gmail.com>
2019-12-16 14:32:20 +0700

    commit

--- resources/views/portofolio/portofolio.blade.php
+++ resources/views/portofolio/portofolio.blade.php
@@ -48,7 +48,7 @@
                                     @foreach($portofolios->gambarWeb as $gambar)                                    
                                         <div class="col-md-12">
                                             <div class="row justify-content-center">
-                                                <img src="{{ URL::to('/') }}/resizedImages/{{ $gambar->gambar_website }}" width="280px" alt=""> 
+                                                <img src="{{ URL::to('/') }}/resizedImages/{{ $gambar->gambar_website }}" class="card-img-top" width="100px" alt=""> 
                                             </div>                                           
                                         </div>                                
                                     @endforeach
